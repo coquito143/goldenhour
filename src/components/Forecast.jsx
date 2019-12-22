49daca0303143e8f45b585175d4ef8e5d01b25d8
Melanie Phan <melanie.phan@prudential.com>
2019-12-22 15:45:06 -0500

    added tablet media query and improved css styling

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -5,26 +5,28 @@ import { round } from '../services/api-helper'
 function Forecast(props) {
   return (
 
-    <div id="forecast-div">
-      <h2>Six Day Forecast for {props.city}</h2>
+    <React.Fragment>
+      <h2 id="forecast-header">Six Day Forecast for {props.city}</h2>
       <h3><em>select a day to view photography conditions</em></h3>
-      {props.forecast.map(day => (
-        <Link to={`/${day.valid_date}`} key={day.valid_date}>
-          <div class="day-div" id={day.valid_date}>
-            <h3>{day.valid_date}</h3>
-            <ul>
-              <li>High: {round(day.high_temp, 0)} &#8457;</li>
-              <li>Low: {round(day.low_temp, 0)} &#8457;</li>
-            </ul>
-            <div class="weather-icon-div">
-              <img src={require(`../img/${day.weather.icon}.png`)} />
-              <p>{day.weather.description}</p>
-            </div>
-          </div >
-        </Link>
-      ))
-      }
-    </div>
+      <div id="forecast-div">
+        {props.forecast.map(day => (
+          <Link to={`/${day.valid_date}`} key={day.valid_date}>
+            <div class="day-div" id={day.valid_date}>
+              <h3>{day.valid_date}</h3>
+              <ul>
+                <li>High: {round(day.high_temp, 0)} &#8457;</li>
+                <li>Low: {round(day.low_temp, 0)} &#8457;</li>
+              </ul>
+              <div class="weather-icon-div">
+                <img src={require(`../img/${day.weather.icon}.png`)} />
+                <p>{day.weather.description}</p>
+              </div>
+            </div >
+          </Link>
+        ))
+        }
+      </div>
+    </React.Fragment>
   )
 }
 
